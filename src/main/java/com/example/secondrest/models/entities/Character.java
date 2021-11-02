4da6eef273e097c8465cceaf6fb82bf1a9ed562a
GODofCAT <dis2827a@gmail.com>
2021-11-02 19:51:05 +0300

    sixth

--- src/main/java/com/example/secondrest/models/entities/Character.java
+++ src/main/java/com/example/secondrest/models/entities/Character.java
@@ -2,7 +2,9 @@ package com.example.secondrest.models.entities;
 
 import javax.persistence.*;
 
+
 @Entity
+@Table(name = "characters")
  public class Character {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
